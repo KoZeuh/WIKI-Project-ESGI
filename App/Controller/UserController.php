3decc380ba989e99edf0f1391f73d1ad25092f5a
antoinebtn <antoinebtn@gmail.com>
2023-12-13 11:43:44 +0100

    création entité User et Article

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -6,7 +6,6 @@ class UserController
 {
     public function index()
     {
-        // Logique pour afficher la liste des utilisateurs
         echo 'Affichage de la liste des utilisateurs';
     }
 }
