346ada6fb56d98890db755efcaa8a05ec4e0d96c
Nikoolaii <nikolailemerre@gmail.com>
2024-01-23 08:31:09 +0100

    Fix version delete

--- App/Controller/Admin/VersionController.php
+++ App/Controller/Admin/VersionController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use App\Model\Repository\ArticleRepository;
 use App\Model\Repository\VersionRepository;
 
 class VersionController
 {
     public function delete($id)
     {
-        $nbVersionForArticle = VersionRepository::getInstance()->getCountVersionsByArticleId($id);
+        $articleId = VersionRepository::getInstance()->getArticleIdByVersionId($id);
+        $nbVersionForArticle = VersionRepository::getInstance()->getCountVersionsByArticleId($articleId);
+        echo $nbVersionForArticle;
         if ($nbVersionForArticle > 1) {
             VersionRepository::getInstance()->deleteVersion($id);
         } else {
-            $articleId = VersionRepository::getInstance()->getArticleIdByVersionId($id);
             VersionRepository::getInstance()->deleteVersion($id);
             ArticleRepository::getInstance()->deleteArticle($articleId);
         }
